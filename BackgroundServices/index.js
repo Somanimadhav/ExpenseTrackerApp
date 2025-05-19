b7f389b4e893971912156d8c9d6d00127bc662cd
Madhav Somani <somanimadhav8875@gmail.com>
2025-05-19 11:11:39 +0530

    working on scheduler

---
+++ BackgroundServices/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cron = require("node-cron");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+mongoose.connect(process.env.DB_CONNECTION).then(() =>{
+        console.log("DB Connection is successful");
+}).catch((err) =>
+{
+    console.log(err);
+});
+
+const run = () =>{
+    cron.schedule('* * * * * *', () =>
+    {
+        console.log("running a task every second");
+    });
+}
+run();
+
+app.listen(process.env.PORT,() =>{
+console.log(`server is running on port ${process.env.PORT}`);
+})
+
+
